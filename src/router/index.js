e58d7677a2530bbef89ba126f19e318d26deaba5
rimin0650 <chris03232000@gmail.com>
2026-02-24 12:22:49 +0900

    [Feat] 게시글 작성 위한 프론트 페이지 작성 게시글 작성 페이지 라우트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'create',
+      path: '/create',
+      meta: {
+        title: '게시물 작성 페이지',
+      },
+      component: () => import('@/views/board/Create.vue'),
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+
+
   ],
 })
 
---
+++ src/views/board/Create.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+
+
+</script>
+
+<template>
+  <div>제목 : <input type="title" /></div><br>
+  <div>내용 : <input type="contents" /></div><br>
+    
+  <button>등록</button>
+</template>
+
+<style scoped></style>
