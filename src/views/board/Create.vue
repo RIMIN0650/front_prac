71e4ee15d66699f2c6ac08b05bf1b4f27c888ee4
rimin0650 <chris03232000@gmail.com>
2026-02-24 12:25:19 +0900

    [Feat] 등록 버튼 동작 확인

--- src/views/board/Create.vue
+++ src/views/board/Create.vue
@@ -2,14 +2,16 @@
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 
-
+const create = async () => {
+    alert("등록 버튼");
+}
 </script>
 
 <template>
   <div>제목 : <input type="title" /></div><br>
   <div>내용 : <input type="contents" /></div><br>
     
-  <button>등록</button>
+  <button @click="create()">등록</button>
 </template>
 
 <style scoped></style>
