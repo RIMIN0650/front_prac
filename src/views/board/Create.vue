4caa6c17d14c93648b7b6cc5a22f99efec33c335
rimin0650 <chris03232000@gmail.com>
2026-02-24 14:06:01 +0900

    [Feat] 게시글 작성 페이지에서 create API 연결

--- src/views/board/Create.vue
+++ src/views/board/Create.vue
@@ -1,15 +1,30 @@
 <script setup>
 import { reactive } from 'vue'
-import { useRouter } from 'vue-router'
+import api from '@/plugins/axiosinterceptor'
+
+const post = reactive ({
+    title: '',
+    contents: '',
+})
 
 const create = async () => {
-    alert("등록 버튼");
+    try {
+        const res = await api.post('/board/create', post)
+
+        if (res.status === 200) {
+            alert("등록 성공")
+        }
+    } catch (e) {
+        console.error(e)
+        alert('등록 실패')
+    }
+
 }
 </script>
 
 <template>
-  <div>제목 : <input type="title" /></div><br>
-  <div>내용 : <input type="contents" /></div><br>
+  <div>제목 : <input v-model="post.title" type="text" /></div><br>
+  <div>내용 : <input v-model="post.contents" type="text" /></div><br>
     
   <button @click="create()">등록</button>
 </template>
